dcec2a8d0ae62e1cf9ba62a996778a8ae4952f46
Tien Nguyen <nguyen.tien@student.greenriver.edu>
2024-02-08 13:20:53 -0800

    commit

---
+++ src/BinarySearchST.java
@@ -0,0 +1,115 @@
+public class BinarySearchST <Key extends Comparable , Value > implements SymbolTable < Key, Value>
+{
+
+    //private fields
+    private Key[] keys; //array of keys
+    private Value[] values; //array of values
+    private int size;
+
+    //constructor
+    public BinarySearchST ( int capacity )
+    {
+        //capacity is the potential space we will use
+        //size is the actual space used
+
+        //wanted to do keys = new Key[capacity], but Java doesnt do it.
+        // so this is a work around way .
+        keys = (Key[]) new Object [capacity];
+        values = (Value[]) new Object [capacity];
+        size = 0 ;
+    }
+
+    private int rank(Key key)
+    {
+        //returns the index of where the key is located
+        int low = 0;  // low index
+        int high = size - 1; //high index
+        while( low <= high)
+        {
+            int mid = low + (high - low) / 2; // find halfway/ mid index.
+            int compare = key.compareTo(keys[mid]);
+
+                if(compare < 0 )
+                {
+                    high = mid - 1 ;
+                }
+                else if (compare > 0 )
+                {
+                    low = mid + 1;
+                }
+                else {
+                    return mid;
+                }
+        }
+        return low; 
+    }
+    /**
+     * Put a key-value pair into the table
+     *
+     * @param key
+     * @param val
+     */
+    @Override
+    public void put(Key key, Value val) {
+
+    }
+
+    /**
+     * Returns the value paired with the given key.
+     *
+     * @param key
+     */
+    @Override
+    public Value get(Key key) {
+        return null;
+    }
+
+    /**
+     * Remove key (and it's value) from the table.
+     *
+     * @param key
+     */
+    @Override
+    public void delete(Key key) {
+        SymbolTable.super.delete(key);
+    }
+
+    /**
+     * Returns true if there is a value paired with a key.
+     *
+     * @param key
+     */
+    @Override
+    public boolean contains(Key key) {
+        return SymbolTable.super.contains(key);
+    }
+
+    /**
+     * Returns true if the table is empty.
+     *
+     * @return
+     */
+    @Override
+    public boolean isEmpty() {
+        return SymbolTable.super.isEmpty();
+    }
+
+    /**
+     * Returns the number of key-value pairs in the table.
+     *
+     * @return
+     */
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Returns an iterator that refers to all the keys
+     * in the table.
+     */
+    @Override
+    public Iterable<Key> keys() {
+        return null;
+    }
+}
---
+++ src/SequentialSearchST.java
@@ -0,0 +1,129 @@
+public class SequentialSearchST <Key, Value>
+        implements SymbolTable <Key, Value> {
+        private Node head;
+        private int size;
+
+    private class Node {
+        Key key;
+        Value value;
+        Node next;
+
+        //constructor
+        public Node(Key key, Value value, Node next)
+        {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    } // end of Node class
+
+    //SequentialSearchST Class constructor
+    public SequentialSearchST()
+    {
+        head = null;
+        size = 0;
+    }
+
+
+
+    /**
+     * Put a key-value pair into the table
+     *
+     * @param key
+     * @param val
+     */
+    @Override
+    public void put(Key key, Value val) {
+        // if key is not in the list, put it up front at the head.
+        //but if key is already in the list, replace the value in that node.
+
+        // loop through the nodes to see if the key is there.
+        Node current = head;
+        while (current != null )
+        {
+            if(key.equals(current.key))
+            {
+                // we found key! So replace the value
+                current.value = val;
+                return; // done
+            }
+            current = current.next;
+        }
+        // if we got here, we know key doesnt exist in list.
+        //so make a new node and put it at the front
+        head = new Node(key, val, head);
+        size++;
+    }
+
+
+    /**
+     * Returns the value paired with the given key.
+     *
+     * @param key
+     */
+    @Override
+    public Value get(Key key) {
+        Node current = head;
+        while(current != null )
+        {
+            if(key.equals(current.key))
+            {
+                //if the key equals return the current value
+                return current.value;
+            }
+            current = current.next;
+        }
+
+        // if the key does not match, i know the key is not in the list.
+        return null;
+    }
+
+    /**
+     * Remove key (and it's value) from the table.
+     *
+     * @param key
+     */
+    @Override
+    public void delete(Key key) {
+        put(key, null ); // lazy deletion .
+    }
+
+    /**
+     * Returns true if there is a value paired with a key.
+     *
+     * @param key
+     */
+    @Override
+    public boolean contains(Key key) {
+        return SymbolTable.super.contains(key);
+    }
+
+    /**
+     * Returns true if the table is empty.
+     *
+     * @return
+     */
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Returns the number of key-value pairs in the table.
+     *
+     * @return
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Returns an iterator that refers to all the keys
+     * in the table.
+     */
+    @Override
+    public Iterable<Key> keys() {
+        return null;
+    }
+}
